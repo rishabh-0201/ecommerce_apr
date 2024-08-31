04a3cc2e5c3daf5083b845abb074f6e499e0cfeb
anupamdasondhi <anupamdasondhi2003@gmail.com>
2024-09-01 00:37:07 +0530

    rishabh ghocu api maker

---
+++ EcommerceAPR_API/DTO/StateDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace EcommerceAPR.DTO
+{
+    public class StateDTO
+    {
+        public int StateId { get; set; }
+        public string StateName { get; set; }
+    }
+}
---
+++ EcommerceAPR_API/DTO/VendorResponseDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace EcommerceAPR.DTO
+{
+    public class VendorResponseDTO
+    {
+        public int VendorId { get; set; }
+        public string VendorName { get; set; }
+        public string ContactPerson { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public int CityId { get; set; }
+        public int StateId { get; set; }
+        public int CompanyId { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
